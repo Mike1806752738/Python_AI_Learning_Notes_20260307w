e68daa8e433e1371a499352df36d80b5e497a90a
Mike1806752738 <1806752738@qq.com>
2026-03-07 17:32:22 +0800

    Add files via upload
    Circle Area Calculator 圆面积计算器 This program calculates the area of a circle based on the radius input by the user. 本程序根据用户输入的半径计算圆的面积

---
+++ circle_area_calculator.py
@@ -0,0 +1,10 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> pi=3.14 # 定义圆周率
+>>> radius=float (input ("请输入圆的半径："))
+请输入圆的半径：9
+>>> area = pi * radius * * 2
+SyntaxError: invalid syntax
+>>> area=pi * radius ** 2
+>>> print ("圆的面积是：",area)
+圆的面积是： 254.34
