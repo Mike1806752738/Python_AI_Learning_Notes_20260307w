770f65ef98a769745557cf3487ba4b4aafa35ef8
mike-Wang <1806752738@qq.com>
2026-03-27 17:54:19 +0800

    Add files via upload
    基于多分支条件的成绩等级分类程序

---
+++ python/MultiBranchGradeClassifier.py
@@ -0,0 +1,17 @@
+# coding = utf-8
+# 代码文件：ch05 MultiBranchGradeClassifier.py
+
+score = int(input("请输入一个0~100的整数:") )
+
+if score >= 90:
+    grade  = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print ("Grade = " + grade)
